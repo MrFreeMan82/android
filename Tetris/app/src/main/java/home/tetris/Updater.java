9f14c96bbb420f9d2271964c2587149c9d692375
MrFreeMan82 <DimaZDY82@yandex.ru>
2017-02-23 15:07:32 +0200

    new version 2

--- Tetris/app/src/main/java/home/tetris/Updater.java
+++ Tetris/app/src/main/java/home/tetris/Updater.java
@@ -1,27 +1,24 @@
 package home.tetris;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
  * Created by Дима on 23.02.2017.
+ *
  */
 
-public class Updater extends AsyncTask<Void, Void, Void>{
+class Updater extends AsyncTask<Void, Void, Void>{
 
     private static final String TAG = "Updater";
     private static final String VERSION_URL = "https://raw.githubusercontent.com/MrFreeMan82/android/master/Tetris/version.txt";
@@ -55,7 +52,7 @@ public class Updater extends AsyncTask<Void, Void, Void>{
             }
 
             int bytesRead;
-            byte[] buffer = new byte[8];
+            byte[] buffer = new byte[1024];
             while((bytesRead = in.read(buffer)) > 0) out.write(buffer, 0, bytesRead);
             out.close();
             return out.toString();
